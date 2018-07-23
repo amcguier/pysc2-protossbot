b349680a8a1829be29a46f2e1fadc8cdff633fca
Xinfu32 <41170034+Xinfu32@users.noreply.github.com>
2018-07-23 01:20:10 -0400

    EconomyBot Final Version

--- protoss/pgss_protoss_agent.py
+++ protoss/pgss_protoss_agent.py
@@ -16,10 +16,12 @@ class ProtossAgent(base_agent.BaseAgent):
     sub_action_number = 0
     number_of_bases = 1
     time_until_nexus = 2000
-    time_supply_needed = 10
-    time_without_idle = 10
+    time_supply_needed = 250
+    time_idle_needed = 250
+    supply_needed = True
+    idle_needed = True
+    build_pylon = False
     stop_worker_production = False
-    main_base_filled = False
     minerals_filled = False
     
     resource_locations = []
@@ -30,8 +32,6 @@ class ProtossAgent(base_agent.BaseAgent):
     nexus_location = [0, 0]
     build_lean = [0, 0]
     first_pylon_location = [0, 0]
-    pylon_area = [[0, 0],
-                  [0, 0]]
     
     geysers = []
     
@@ -100,18 +100,13 @@ class ProtossAgent(base_agent.BaseAgent):
             xmean = player_x.mean()
             ymean = player_y.mean()
             if xmean < self.MINIMAP_DIM / 2:
-                self.pylon_area[1][0] = 0
                 self.build_lean[0] = -1
             else:
-                self.pylon_area[1][0] = 69
                 self.build_lean[0] = 1
-                
             if ymean < self.MINIMAP_DIM / 2:
-                self.pylon_area[1][1] = 0
                 self.build_lean[1] = -1
             else:
-                self.pylon_area[1][1] = 69
-                self.build_lean[1] = 1    
+                self.build_lean[1] = 1
             for i in range(number_of_clusters):
                 if abs(self.resource_locations[i][1] - ymean) < 10:
                     if abs(self.resource_locations[i][0] - xmean) < 10:
@@ -125,9 +120,6 @@ class ProtossAgent(base_agent.BaseAgent):
                         self.main_enemy_base = self.resource_locations[i]
                     else:
                         self.possible_enemy_base = self.resource_locations[i]
-        
-        
-        
         
         
         nexi = self.get_units_by_type(obs, units.Protoss.Nexus)
@@ -157,24 +149,29 @@ class ProtossAgent(base_agent.BaseAgent):
         
         
         
-        
+        print(obs.observation.last_actions)
         
         self.sub_action_number+=1
         if self.number_of_bases == 2 and self.time_until_nexus > 0:
             self.time_until_nexus-=1
-        
-        
+        if self.idle_needed == False:
+            self.time_idle_needed = 0
+        else:
+            self.time_idle_needed+=1
+        if self.supply_needed == False:
+            self.time_supply_needed = 0
+        else:
+            self.time_supply_needed+=1
+            
+            
         
         #Select the main base and train and send workers to resources
         if self.action_number == 1:
             
-            if self.sub_action_number == 1:
+            if self.sub_action_number == 2:
                 return actions.FUNCTIONS.move_camera(self.main_base_camera)
             
-            if self.sub_action_number == 2:
-                return actions.FUNCTIONS.no_op()
-            
-            if self.sub_action_number == 3:
+            if self.sub_action_number == 4:
                 if len(self.geysers) == 0:
                     self.geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
                 if self.number_of_bases == 1 and len(nexi) > 0:
@@ -183,46 +180,36 @@ class ProtossAgent(base_agent.BaseAgent):
                 if len(nexi) > 0:
                     return actions.FUNCTIONS.select_point("select", self.nexus_location)
             
-            if self.sub_action_number == 4:
+            if self.sub_action_number == 5:
                 if self.unit_type_is_selected(obs, units.Protoss.Nexus):
                     if self.can_do(obs, actions.FUNCTIONS.select_idle_worker.id):
-                        self.time_without_idle = 0
+                        self.idle_needed = False
                     else:
-                        self.time_without_idle+=1
-                        if self.time_without_idle > 10:
+                        self.idle_needed = True
+                        if self.time_idle_needed > 250:
                             return actions.FUNCTIONS.Rally_Workers_screen("now", (42, 42))
                     if nexus.assigned_harvesters < nexus.ideal_harvesters:
                         return actions.FUNCTIONS.Rally_Workers_screen("now", (minerals[0].x, minerals[0].y))
                     for i in range(len(assimilators)):
                         if assimilators[i].assigned_harvesters < assimilators[i].ideal_harvesters and assimilators[i].ideal_harvesters != 0:
                             return actions.FUNCTIONS.Rally_Workers_screen("now", (assimilators[i].x, assimilators[i].y))
-                    if self.number_of_bases == 1:
-                        self.main_base_filled = True
-                        if self.can_do(obs, actions.FUNCTIONS.Cancel_Last_quick.id):
-                            return actions.FUNCTIONS.Cancel_Last_quick("now")
-                    if self.minerals_filled:
+                    if self.number_of_bases == 1 or self.minerals_filled:
                         if self.can_do(obs, actions.FUNCTIONS.Cancel_Last_quick.id):
                             return actions.FUNCTIONS.Cancel_Last_quick("now")
                             
-            if self.sub_action_number == 5:
-                print(self.minerals_filled)
-                print(len(obs.observation.build_queue))
+            if self.sub_action_number == 6:
                 if self.number_of_bases == 1:
                     self.sub_action_number = 0
                     self.action_number = 2
-                if self.minerals_filled == False and (self.main_base_filled == False or self.number_of_bases > 1):
-                    if self.unit_type_is_selected(obs, units.Protoss.Nexus):
-                        if len(obs.observation.build_queue) <= 1:
-                            if self.can_do(obs, actions.FUNCTIONS.Train_Probe_quick.id):
-                                return actions.FUNCTIONS.Train_Probe_quick("now")
-            
-            if self.sub_action_number == 6:
-                return actions.FUNCTIONS.move_camera(self.natural_base_camera)
-            
-            if self.sub_action_number == 7:
-                return actions.FUNCTIONS.no_op()
+                if self.unit_type_is_selected(obs, units.Protoss.Nexus):
+                    if len(obs.observation.build_queue) <= 1:
+                        if self.can_do(obs, actions.FUNCTIONS.Train_Probe_quick.id):
+                            return actions.FUNCTIONS.Train_Probe_quick("now")
             
             if self.sub_action_number == 8:
+                return actions.FUNCTIONS.move_camera(self.natural_base_camera)
+            
+            if self.sub_action_number == 10:
                 self.sub_action_number = 0
                 self.action_number = 2
                 if self.unit_type_is_selected(obs, units.Protoss.Nexus):
@@ -236,32 +223,27 @@ class ProtossAgent(base_agent.BaseAgent):
                 if self.can_do(obs, actions.FUNCTIONS.select_idle_worker.id):
                     return actions.FUNCTIONS.select_idle_worker("select")
             
-            if self.sub_action_number == 2:
-                return actions.FUNCTIONS.move_camera(self.main_base_camera)
-                
             if self.sub_action_number == 3:
-                return actions.FUNCTIONS.no_op()
+                return actions.FUNCTIONS.move_camera(self.main_base_camera)
+                                    
+            if self.sub_action_number == 5:
+                if len(pylons) > 0 and len(assimilators) < len(self.geysers):
+                    if self.unit_type_is_selected(obs, units.Protoss.Probe):
+                        if self.can_do(obs, actions.FUNCTIONS.Build_Assimilator_screen.id):
+                            return actions.FUNCTIONS.Build_Assimilator_screen("queued", (self.geysers[len(assimilators)].x, self.geysers[len(assimilators)].y))
             
-            if self.sub_action_number == 4:
+            if self.sub_action_number == 6:
                 if self.unit_type_is_selected(obs, units.Protoss.Probe):
                     if obs.observation.player.food_cap - obs.observation.player.food_used < 20 and obs.observation.player.food_cap != 200:
-                        self.time_supply_needed+=1
-                        if self.time_supply_needed > 10:
+                        self.supply_needed = True
+                        if self.time_supply_needed > 250:
                             if self.can_do(obs, actions.FUNCTIONS.Build_Pylon_screen.id):
-                                self.time_supply_needed = 0
+                                self.supply_needed = False
                                 if len(pylons) == 0:
                                     unit_type = obs.observation["feature_screen"][features.SCREEN_FEATURES.unit_type.index]
                                     mineral_y, mineral_x = (unit_type == units.Neutral.MineralField).nonzero()
-                                    dx = int(round(mineral_x.mean()))
-                                    dy = int(round(mineral_y.mean()))
-                                    x = nexus.x
-                                    y = nexus.y
-                                    self.pylon_area[0][0] = dx
-                                    self.pylon_area[0][1] = dy - 40*self.build_lean[1]
-                                    dx-=x
-                                    dy-=y
-                                    x-=(dx + 5*self.build_lean[0])
-                                    y-=(dy + 5*self.build_lean[1])
+                                    x = 2*nexus.x - int(round(mineral_x.mean())) - 5*self.build_lean[0]
+                                    y = 2*nexus.y - int(round(mineral_y.mean())) - 5*self.build_lean[1]
                                     self.first_pylon_location[0] = x
                                     self.first_pylon_location[1] = y
                                     return actions.FUNCTIONS.Build_Pylon_screen("queued", self.first_pylon_location)
@@ -270,37 +252,43 @@ class ProtossAgent(base_agent.BaseAgent):
                                 elif len(pylons) == 2:
                                     return actions.FUNCTIONS.Build_Pylon_screen("queued", (self.first_pylon_location[0], self.first_pylon_location[1] + self.build_lean[1]*35))
                                 else:
-                                    if(self.pylon_area[1][0] < self.pylon_area[0][0]):
-                                        return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(self.pylon_area[1][0], self.pylon_area[0][0]), random.randint(self.pylon_area[1][1], self.pylon_area[0][1])))
-                                    else:
-                                        return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(self.pylon_area[0][0], self.pylon_area[1][0]), random.randint(self.pylon_area[0][1], self.pylon_area[1][1])))
+                                    self.build_pylon = True
+                                    return actions.FUNCTIONS.move_camera(self.natural_base_camera)
                                     
-            if self.sub_action_number == 5:
-                self.sub_action_number = 0
-                if self.time_until_nexus == 0:
-                    self.action_number = 3
-                elif obs.observation.player.minerals > 550 and self.number_of_bases == 1:
-                    self.action_number = 10
+            if self.sub_action_number == 8:
+                if (len(obs.observation.last_actions) > 0 and obs.observation.last_actions[0] == 70) or self.build_pylon == False:
+                    self.build_pylon = False
+                    self.sub_action_number = 0
+                    if self.time_until_nexus == 0:
+                        self.action_number = 3
+                    elif obs.observation.player.minerals > 550 and self.number_of_bases == 1:
+                        self.action_number = 10
+                    else:
+                        self.action_number = 1
                 else:
-                    self.action_number = 1
-                if len(pylons) > 0 and len(assimilators) < len(self.geysers):
-                    if self.unit_type_is_selected(obs, units.Protoss.Probe):
-                        if self.can_do(obs, actions.FUNCTIONS.Build_Assimilator_screen.id):
-                            return actions.FUNCTIONS.Build_Assimilator_screen("queued", (self.geysers[len(assimilators)].x, self.geysers[len(assimilators)].y))
-            
-            
+                    self.sub_action_number-=1
+                if self.build_pylon:
+                    if random.randint(0, 1) == 0:
+                        if self.build_lean[0] == -1:
+                            return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(0, 28), random.randint(0, 69)))
+                        else:
+                            return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(55, 83), random.randint(0, 69)))
+                    else:
+                        if self.build_lean[1] == -1:
+                            return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(0, 83), random.randint(55, 69)))
+                        else:
+                            return actions.FUNCTIONS.Build_Pylon_screen("queued", (random.randint(0, 83), random.randint(0, 28)))
+                
         #Pan camera to natural expansion to train and send workers to resources
         if self.action_number == 3:
-            if self.sub_action_number == 1:
-                return actions.FUNCTIONS.move_camera(self.natural_base_camera)
             
             if self.sub_action_number == 2:
-                return actions.FUNCTIONS.no_op()
+                return actions.FUNCTIONS.move_camera(self.natural_base_camera)
             
-            if self.sub_action_number == 3:
+            if self.sub_action_number == 4:
                 return actions.FUNCTIONS.select_point("select", (42, 42))
                 
-            if self.sub_action_number == 4:
+            if self.sub_action_number == 5:
                 if self.unit_type_is_selected(obs, units.Protoss.Nexus):
                     if nexus.assigned_harvesters < nexus.ideal_harvesters:
                         return actions.FUNCTIONS.Rally_Workers_screen("now", (minerals[0].x, minerals[0].y))
@@ -313,17 +301,35 @@ class ProtossAgent(base_agent.BaseAgent):
                         self.stop_worker_production = True
                         return actions.FUNCTIONS.Cancel_Last_quick("now")
                             
-            if self.sub_action_number == 5:
-                print(len(obs.observation.build_queue))
+            if self.sub_action_number == 6:
                 self.sub_action_number = 0
                 self.action_number = 1
-                if self.stop_worker_production == False:
-                    if self.unit_type_is_selected(obs, units.Protoss.Nexus):
-                        if len(obs.observation.build_queue) <= 1:
-                            if self.can_do(obs, actions.FUNCTIONS.Train_Probe_quick.id):
-                                return actions.FUNCTIONS.Train_Probe_quick("now")
+                if self.unit_type_is_selected(obs, units.Protoss.Nexus):
+                    if len(obs.observation.build_queue) <= 1:
+                        if self.can_do(obs, actions.FUNCTIONS.Train_Probe_quick.id):
+                            return actions.FUNCTIONS.Train_Probe_quick("now")
                             
-                            
+        
+        #Construct combat buildings
+        if self.action_number == 4:
+            print("lol")
+        
+        #Research tech from forge
+        if self.action_number == 5:
+            print("lol")
+        
+        #Research warpgates from cybernetics core, charge and blink from twilight council, and psionic storm from templar archive
+        if self.action_number == 6:
+            print("lol")
+        
+        #Scouting
+        if self.action_number == 7:
+            print("lol")
+        
+        #Combat
+        if self.action_number == 8:
+            print("lol")
+        
         #Select worker, pan camera to natural expansion, and build nexus and assimilators
         if self.action_number == 10:
             
@@ -332,24 +338,21 @@ class ProtossAgent(base_agent.BaseAgent):
                 if self.can_do(obs, actions.FUNCTIONS.select_idle_worker.id):
                     return actions.FUNCTIONS.select_idle_worker("select")
                     
-            if self.sub_action_number == 2:
+            if self.sub_action_number == 3:
                 return actions.FUNCTIONS.move_camera(self.natural_base_camera)
             
-            if self.sub_action_number == 3:
-                return actions.FUNCTIONS.no_op()
-            
-            if self.sub_action_number == 4:
+            if self.sub_action_number == 5:
                 if self.unit_type_is_selected(obs, units.Protoss.Probe):
                     if self.can_do(obs, actions.FUNCTIONS.Build_Nexus_screen.id):
                         return actions.FUNCTIONS.Build_Nexus_screen("queued", (42, 42))
                 
-            if self.sub_action_number == 5:
+            if self.sub_action_number == 6:
                 self.geysers = self.get_units_by_type(obs, units.Neutral.VespeneGeyser)
                 if self.unit_type_is_selected(obs, units.Protoss.Probe):
                     if self.can_do(obs, actions.FUNCTIONS.Build_Assimilator_screen.id):
                         return actions.FUNCTIONS.Build_Assimilator_screen("queued", (self.geysers[0].x, self.geysers[0].y))
                 
-            if self.sub_action_number == 6:
+            if self.sub_action_number == 7:
                 self.sub_action_number = 0
                 self.action_number = 1
                 if self.unit_type_is_selected(obs, units.Protoss.Probe):
@@ -360,40 +363,32 @@ class ProtossAgent(base_agent.BaseAgent):
                             
         return actions.FUNCTIONS.no_op()
 
+
+
+
+
 def main(unused_argv):
     agent = ProtossAgent()
     try:
         while True:
             with sc2_env.SC2Env(
-                    
                     #Map selection
                     map_name="Simple64",
-                    
                     #Players
                     players=[sc2_env.Agent(sc2_env.Race.protoss),
-                                     sc2_env.Bot(sc2_env.Race.random,
-                                                             sc2_env.Difficulty.very_easy)],
-                    
-                    #Define map and minimap size and enable feature units
+                             sc2_env.Bot(sc2_env.Race.random,
+                                         sc2_env.Difficulty.very_easy)],
+                    #Define map and minimap size
                     agent_interface_format=features.AgentInterfaceFormat(
                             feature_dimensions=features.Dimensions(screen=84, minimap=64),
                             use_feature_units=True),
-                            
-                    #Number of steps that will pass before bot makes an action
                     step_mul=1,
-                    
-                    #Unlimited game length
                     game_steps_per_episode=0,
-                    
-                    #GUI
                     visualize=True) as env:
                 
-                #Looping code
                 agent.setup(env.observation_spec(), env.action_spec())
-                
                 timesteps = env.reset()
                 agent.reset()
-                
                 while True:
                     step_actions = [agent.step(timesteps[0])]
                     if timesteps[0].last():
