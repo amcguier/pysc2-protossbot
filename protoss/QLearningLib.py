b3fc863533bb4d040d47c14a4d8cf2508bd03231
Evan <evantroop@gmail.com>
2018-07-30 10:55:16 -0400

    Created CSV for legions and overall units
    Created the csvs we needed

--- protoss/QLearningLib.py
+++ protoss/QLearningLib.py
@@ -23,18 +23,55 @@ import Feature as f
 import MachineLearningDriver as mld
 import random as rd
 
-def generate_csv(name): 
-    #pre_q = pd.Dataframe(
-    #states = np.
-    print()
+def generate_csv(name, type_list='BASIC'): 
+    if type_list == 'BASIC':
+        state_list = []
+        for p1 in range(1,6):
+            for p2 in range(1,6):
+                for p3 in range(1,6):
+                    for p4 in range(1,6):
+                        for p5 in range(1,6):
+                            n = 10000*p1  +1000*p2 + 100*p3 + 10*p4 + p5
+                            state_list.append(n)
+                        
+        action_list = [1,2,3,4,5]
     
+        frame = pd.DataFrame(0,index=state_list, columns=action_list)
+        frame.to_csv(name)
+        print('CSV Created : ' + name)
+        frame.describe()
+        return True
+    return False
+   
+    
+scales = [[1,2,4,8,16], #Zealot
+          [2,4,8,16,32], #Stalker
+          [0,1,2,4,8], #Immortal
+          [0,1,2,4,8], #Sentury
+          [2700,5400,8100,13500,27000]] #Time
+
+def get_scaled_value(typ, n_zealot=0, n_stalker=0, n_immortal=0, n_sentury=0, time=0):
+    if typ == 'SIMPLE':
+        numbers = [n_zealot, n_stalker, n_immortal, n_sentury, time]
+        outs = [0,0,0,0,0]
+        for i in range(len(numbers)):
+            for j in range(len(scales[i])):
+                if scales[i][j] > numbers[i]:
+                    outs[i] = j + 1
+                    break
+            if(outs[i] == 0):
+                outs[i] = 5
+                
+        return outs
+        
+
 class Q_list():
     
     def __init__(self, path, mode):
         #rows(index) = states
         #columns is actions
         
-        #
+        
         
         # will store past actions
         self.past_actions = pd.DataFrame(index=[], columns=['state', 'action'])
@@ -74,15 +111,23 @@ class Q_list():
                 
                 current_val = self.q_list.loc[st][ac]
                 current_val = current_val + reward * (self.gamma ** (len(self.past_actions) - i))
-                print(current_val)
+                
                
         if state in self.q_list.index and action in self.q_list.columns:
             self.q_list.loc[state][action] = reward
+            self.past_actions.append(pd.DataFrame)
             return True
         
         return False
-            
+    
+    
+    def export_q_list(self):
+        self.q_list.to_csv(self.path, index=False)
         
+        
+    def end_of_game(self):
+        self.export_q_list(self)
+        print("End of Game Funcitons Complete")
 
 #zs = pd.DataFrame(0, index=['state1','state2','state3','state4'], columns=['action1','action2'])
 
@@ -98,7 +143,7 @@ print(zs)
 print()
 print(m)
 """
-
+"""
 past_actions = pd.DataFrame(index=[], columns=['action', 'state'])
 
 past_actions = past_actions.append(pd.DataFrame.from_dict({'action' : ['hello1'], 'state':['hello1']}), ignore_index=True)
@@ -114,4 +159,7 @@ print(si)
 for i in range(len(past_actions)):
     print(past_actions.loc[i]['action'])
 print(past_actions)
-    
+"""
+
+
+
